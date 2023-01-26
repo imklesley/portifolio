90a3c8e3082471b944512daaa0bc574cb0c592ba
Klesley <imklesley@gmail.com>
2023-01-25 22:46:33 -0300

    Responsiviness and actions on drawer done!

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 // Change Header background when scroll down
-let header =  document.querySelector('header');
+let header = document.querySelector("header");
 
 // Show Scroll to top button when achive height 500
-let scrollToTopButton =  document.querySelector('.scroll-to-top');
+let scrollToTopButton = document.querySelector(".scroll-to-top");
 
 // when scroll down will be added the class active-header, that have glass style
-window.addEventListener('scroll',(e)=>{
-    header.classList.toggle('active-header', scrollY > 5);
-    scrollToTopButton.classList.toggle('active-scroll-to-top', scrollY > 500);
+window.addEventListener("scroll", (e) => {
+  //Só vai realizar a alteração se o width for maior que 800, para não desfazer o glasmorphism do mobile
+  if (window.innerWidth > 800) {
+    //Active a glassmorphism background header
+    header.classList.toggle("active-header", scrollY > 5);
+  }
+  //Show scroll to top button on screen
+  scrollToTopButton.classList.toggle("active-scroll-to-top", scrollY >= 250);
 });
+
 
--- js/menu-icon.js
+++ js/menu-icon.js
@@ -1,5 +1,26 @@
 const menuIcon = document.querySelector(".menu-icon");
+const navbar =  document.querySelector('nav > ul');
+const emptySpaceOnOpenDrawer =  document.querySelector('.empty-space-on-open-drawer');
 
 menuIcon.addEventListener("click", () => {
   menuIcon.classList.toggle("move");
+  navbar.classList.toggle('active-nav');
+  emptySpaceOnOpenDrawer.classList.toggle('empty-space-on-open-drawer-active')
 });
+
+
+//Foi colocado esse espaço vazio para uando o usuário clicar fora do drawer
+// fará ele fechar
+emptySpaceOnOpenDrawer.addEventListener('click',()=>{
+  menuIcon.classList.toggle("move");
+  navbar.classList.toggle('active-nav');
+  emptySpaceOnOpenDrawer.classList.toggle('empty-space-on-open-drawer-active')
+})
+
+
+// Com o drawer aberto quando o usuário scrolar ele será fechado
+window.onscroll = ()=>{
+  menuIcon.classList.remove("move");
+  navbar.classList.remove('active-nav');
+  emptySpaceOnOpenDrawer.classList.remove('empty-space-on-open-drawer-active')
+}
