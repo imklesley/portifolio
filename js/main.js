86c68e31221b2d92f151b7588e9e1be2a5726bee
Klesley <imklesley@gmail.com>
2023-01-25 22:53:27 -0300

    Correction on glassmorphism on drawer

--- js/main.js
+++ js/main.js
@@ -6,11 +6,10 @@ let scrollToTopButton = document.querySelector(".scroll-to-top");
 
 // when scroll down will be added the class active-header, that have glass style
 window.addEventListener("scroll", (e) => {
-  //Só vai realizar a alteração se o width for maior que 800, para não desfazer o glasmorphism do mobile
-  if (window.innerWidth > 800) {
+  
     //Active a glassmorphism background header
-    header.classList.toggle("active-header", scrollY > 5);
-  }
+    header.classList.toggle("active-header", scrollY > 5 && window.innerWidth > 800);
+
   //Show scroll to top button on screen
   scrollToTopButton.classList.toggle("active-scroll-to-top", scrollY >= 250);
 });
